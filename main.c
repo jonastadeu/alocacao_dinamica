4b5edd8609873890a9d16c3be18d7cc53b79bf24
jonastadeu <jonasreichert@gmail.com>
2016-09-16 16:56:04 -0300

    programa formatado

--- main.c
+++ main.c
@@ -5,14 +5,14 @@
 int main(int argc, char *argv[]) {
 	// cria um ponteiro para o bloco de memoria alocado
 	char *memoria_alocada;
-    // aloca a quantidade de memoria solicitada (20 x char)
+    // aloca a quantidade de memoria solicitada (50 x char)
     // e inicializa o ponteiro com a posicao da memoria
     memoria_alocada = calloc (50, sizeof (char));
     // se o ponteiro foi inicializado com o valor nulo
-    if(memoria_alocada == NULL)
+    if (memoria_alocada == NULL)
     {
     	// exibe a mensagem de erro
-    	printf("Nao foi possivel alocar a memoria desejada!\n");
+    	printf ("Nao foi possivel alocar a memoria desejada!\n");
     }
     // se o ponteiro nao for nulo
     else
@@ -21,9 +21,10 @@ int main(int argc, char *argv[]) {
     	strcpy (memoria_alocada, "Esta e a melhor aula do mundo!\n");
     }
     // exibe o conteudo da memoria alocada
-    printf("O conteudo da memoria alocada e: %s\n", memoria_alocada );
-    // libera a memoria alocada
-    free(memoria_alocada);
+    printf ("O conteudo da memoria alocada e: %s\n", memoria_alocada);
+    
+	// libera a memoria alocada
+    free (memoria_alocada);
     // retorna 0
 	return 0;
 }
