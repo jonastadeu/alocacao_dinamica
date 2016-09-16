7655b003bf206e6b8b72cc2201ae262ea2628e7b
jonastadeu <jonasreichert@gmail.com>
2016-09-16 16:51:42 -0300

    programa funcional e formatado

--- main.c
+++ main.c
@@ -7,12 +7,12 @@ int main(int argc, char *argv[]) {
 	char *memoria_alocada;
     // aloca a quantidade de memoria solicitada (20 x char)
     // e inicializa o ponteiro com a posicao da memoria
-    memoria_alocada = calloc (50, sizeof (char));
+    memoria_alocada = malloc (50 * sizeof (char));
     // se o ponteiro foi inicializado com o valor nulo
-    if(memoria_alocada == NULL)
+    if (memoria_alocada == NULL)
     {
     	// exibe a mensagem de erro
-    	printf("Nao foi possivel alocar a memoria desejada!\n");
+    	printf ("Nao foi possivel alocar a memoria desejada!\n");
     }
     // se o ponteiro nao for nulo
     else
@@ -21,9 +21,35 @@ int main(int argc, char *argv[]) {
     	strcpy (memoria_alocada, "Esta e a melhor aula do mundo!\n");
     }
     // exibe o conteudo da memoria alocada
-    printf("O conteudo da memoria alocada e: %s\n", memoria_alocada );
+    printf ("O conteudo da memoria alocada e: %s\n", memoria_alocada);
+    
+    // OBS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	// exibe a quantidade de memoria alocada
+	//printf ("Quantidade de memoria alocada: %i\n", strlen (memoria_alocada));
+    
+    // altera a quantidade de memoria alocada para (100 x char)
+    memoria_alocada = realloc (memoria_alocada, 100 * sizeof (char));
+    // se o ponteiro foi inicializado com o valor nulo
+    if (memoria_alocada == NULL)
+    {
+    	// exibe a mensagem de erro
+    	printf ("Nao foi possivel alocar a memoria desejada!\n");
+    }
+    // se o ponteiro nao for nulo
+    else
+    {
+    	// transfere a mensagem para a memoria alocada
+    	strcpy (memoria_alocada, "Quantidade de memoria alocada alterada!\n");
+    }
+    // exibe o conteudo da memoria alocada
+    printf ("O conteudo da memoria alocada e: %s\n", memoria_alocada);
+    
+    // OBS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // exibe a quantidade de memoria alocada
+    //printf ("Quantidade de memoria alocada: %i\n", strlen (memoria_alocada));
+    
     // libera a memoria alocada
-    free(memoria_alocada);
+    free (memoria_alocada);
     // retorna 0
 	return 0;
 }
