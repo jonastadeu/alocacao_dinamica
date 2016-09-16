763186981a15e7285a8d0764b01b952f12aaccc0
jonastadeu <jonasreichert@gmail.com>
2016-09-16 16:23:12 -0300

    programa funcional

--- main.c
+++ main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) {
+	// cria um ponteiro para o bloco de memoria alocado
+	char *memoria_alocada;
+    // aloca a quantidade de memoria solicitada (20 x char)
+    // e inicializa o ponteiro com a posicao da memoria
+    memoria_alocada = malloc (50 * sizeof (char));
+    // se o ponteiro foi inicializado com o valor nulo
+    if(memoria_alocada == NULL)
+    {
+    	// exibe a mensagem de erro
+    	printf("Nao foi possivel alocar a memoria desejada!\n");
+    }
+    // se o ponteiro nao for nulo
+    else
+    {
+    	// transfere a mensagem para a memoria alocada
+    	strcpy (memoria_alocada, "Esta e a melhor aula do mundo!\n");
+    }
+    // exibe o conteudo da memoria alocada
+    printf("O conteudo da memoria alocada e: %s\n", memoria_alocada );
+    // libera a memoria alocada
+    free(memoria_alocada);
+    // retorna 0
 	return 0;
 }
